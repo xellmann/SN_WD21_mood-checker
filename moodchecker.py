240b981bb32b0b19dd98aaca46670c85cdbc22a7
Ivo Gsellmann <ivo.gsellmann@gmx.at>
2021-04-06 09:06:57 +0200

    initial commit

---
+++ moodchecker.py
@@ -0,0 +1,15 @@
+
+mood = str(input("What mood are you in [happy, nervous, sad, excited, relaxed]: "))
+
+if mood == "happy":
+    print("It is great to see you happy!")
+elif mood == "nervous":
+    print("Take a deep breath 3 times.")
+elif mood == "sad":
+    print("Cheer up maaaaate.")
+elif mood == "excited":
+    print("Go for it!")
+elif mood == "relaxed":
+    print("chiiiiill mate.")
+else:
+    print("I don't recognize this mood.")
